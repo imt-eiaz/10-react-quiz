11f981c5542e576a2953617c2d8ed2fecb04edd2
imt-eiaz <imteiaz@gmail.com>
2025-09-18 15:52:15 +0100

    10. Handling New Answers

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,22 @@
-function Options({ question }) {
+import { type } from "@testing-library/user-event/dist/type";
+
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          disabled={hasAnswered}
+        >
           {option}
         </button>
       ))}
